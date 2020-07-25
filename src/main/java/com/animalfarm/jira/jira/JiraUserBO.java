78b67b1a6670e1a42c18f735c87e0bfa1a2231e2
xiaogege <sch12635@gmail.com>
2020-07-25 18:36:34 +0900

    디비 연결 시도중

--- src/main/java/com/animalfarm/jira/jira/JiraUserBO.java
+++ src/main/java/com/animalfarm/jira/jira/JiraUserBO.java
@@ -1,11 +1,10 @@
 package com.animalfarm.jira.jira;
 
-import org.springframework.stereotype.Service;
-
 /**
  * Jira user business object
  */
-public interface JiraUserBO {
+public interface JiraUserBO
+{
 
 	/**
 	 * 하나의 유저 정보를 가지고 온다
@@ -13,7 +12,7 @@ public interface JiraUserBO {
 	 * @return
 	 */
 	public JiraUser getUser(int memId);
-	
+
 	/**
 	 * JiraUser정보를 디비에 저장한다
 	 * @param jiraUser
--- src/main/java/com/animalfarm/jira/jira/JiraUserDaoImpl.java
+++ src/main/java/com/animalfarm/jira/jira/JiraUserDaoImpl.java
@@ -1,26 +1,37 @@
 package com.animalfarm.jira.jira;
 
-import javax.annotation.Resource;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.springframework.stereotype.Service;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
-public class JiraUserDaoImpl implements JiraUserDao {
-
-	@Resource
-	private SessionFactory sessionFactory;
+public class JiraUserDaoImpl implements JiraUserDao
+{
+	static SessionFactory factory;
+	static Session session;
 	
+	/** * hibernate.cfg.xml의 설정을 읽어서 * db와 연결하는 설정 */
+	static
+	{
+		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+			.applySettings(configuration.getProperties());
+		factory = configuration.buildSessionFactory(builder.build());
+		session = factory.openSession();
+	}
+
 	@Override
-	public JiraUser getJiraUser(int memId) {
-		Session session = sessionFactory.openSession();
+	public JiraUser getJiraUser(int memId)
+	{
+		Session session = factory.openSession();
 		JiraUser list = (JiraUser) session.createCriteria(JiraUser.class).list();
 		return list;
 	}
 
 	@Override
-	public void setJiraUser(JiraUser jiraUser) {
-		
+	public void setJiraUser(JiraUser jiraUser)
+	{
+
 	}
 
 }
